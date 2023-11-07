f912e7211c0a93e5f0d341d4ae74ce7d147fa56f
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-08 01:48:51 +0600

    added some conditional rendering to allServices page

--- src/Pages/AllServices/AllServices.jsx
+++ src/Pages/AllServices/AllServices.jsx
@@ -3,6 +3,7 @@ import SingleServiceCard from "../../components/SingleServiceCard/SingleServiceC
 
 const AllServices = () => {
   const [services, setServices] = useState([]);
+  const [isShow, setIsShow] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost:5000/services")
@@ -13,13 +14,32 @@ const AllServices = () => {
   return (
     <div className="container mx-auto py-36">
       <div className="px-5 grid grid-cols-1 xl:grid-cols-2 gap-6">
-        {services.map((service) => (
-          <SingleServiceCard
-            key={service._id}
-            service={service}
-          ></SingleServiceCard>
-        ))}
+        {isShow
+          ? services?.map((service) => (
+              <SingleServiceCard
+                key={service._id}
+                service={service}
+              ></SingleServiceCard>
+            ))
+          : services
+              ?.slice(0, 6)
+              .map((service) => (
+                <SingleServiceCard
+                  key={service._id}
+                  service={service}
+                ></SingleServiceCard>
+              ))}
       </div>
+      {services?.length > 6 && (
+        <div className="flex justify-center mt-10">
+          <button
+            onClick={() => setIsShow(!isShow)}
+            className="text-white uppercase font-semibold btn mt-5 rounded-lg py-3 px-10 bg-[#E39C0D] hover:bg-[#698F3F]"
+          >
+            {isShow ? "Show Less" : "Show all"}
+          </button>
+        </div>
+      )}
     </div>
   );
 };
