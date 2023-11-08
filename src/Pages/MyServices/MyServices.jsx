cc67df01ac16744e1434f49d02f3d2306ae7fb92
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-08 19:45:17 +0600

    get user's added services from the server

--- src/Pages/MyServices/MyServices.jsx
+++ src/Pages/MyServices/MyServices.jsx
@@ -1,5 +1,22 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+
 const MyServices = () => {
-  return <div>my services page</div>;
+  const { user } = useAuth();
+  const [userServices, setUserServices] = useState([]);
+
+  const url = `http://localhost:5000/usersService?email=${user?.email}`;
+
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setUserServices(res.data);
+    });
+  }, [url]);
+
+  console.log(userServices);
+
+  return <div>{userServices.length}</div>;
 };
 
 export default MyServices;
