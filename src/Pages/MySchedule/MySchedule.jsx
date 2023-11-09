8fae956f1fd15baf52e19149b509ab79dd3a88cc
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-11-09 06:39:51 +0600

    get user's added services booking by other users

--- src/Pages/MySchedule/MySchedule.jsx
+++ src/Pages/MySchedule/MySchedule.jsx
@@ -6,14 +6,22 @@ import MyBookingCard from "../../components/MyBookingCard/MyBookingCard";
 const MySchedule = () => {
   const { user } = useAuth();
   const [userBookings, setUserBookings] = useState([]);
+  const [otherUserBookings, setOtherUserBookings] = useState([]);
 
   const url = `http://localhost:5000/usersBooking?email=${user?.email}`;
+  const url2 = `http://localhost:5000/otherUsersBooking?email=${user?.email}`;
 
   useEffect(() => {
     axios.get(url).then((res) => {
       setUserBookings(res.data);
     });
   }, [url]);
+
+  useEffect(() => {
+    axios.get(url2).then((res) => {
+      setOtherUserBookings(res.data);
+    });
+  }, [url2]);
 
   return (
     <div className="container mx-auto py-32">
@@ -23,7 +31,7 @@ const MySchedule = () => {
           My Bookings
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {userBookings.map((booking) => (
+          {userBookings?.map((booking) => (
             <MyBookingCard key={booking._id} booking={booking}></MyBookingCard>
           ))}
         </div>
@@ -33,6 +41,39 @@ const MySchedule = () => {
         <h2 className="font-bold font-Cabin text-4xl text-center mb-12">
           My Pending works
         </h2>
+        {/* cards container */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {otherUserBookings?.map((booking) => (
+            <div
+              key={booking._id}
+              className="grid grid-cols-2 p-4 bg-white border rounded-xl h-full shadow-sm"
+            >
+              <div className="col-span-1 overflow-hidden">
+                <img
+                  className="rounded-l-xl w-full h-full object-cover"
+                  src={booking.serviceImage}
+                  alt={`image for ${booking.serviceName}`}
+                />
+              </div>
+              <div className="col-span-1">
+                <div className="p-4 flex flex-col h-full justify-center">
+                  <div className="">
+                    <div>
+                      <p className="text-[#270f0f] font-semibold mt-1">
+                        <span className="font-bold">Service Name:</span>{" "}
+                        {booking.serviceName}
+                      </p>
+                      <h6 className="text-[#270f0f] font-semibold mt-1">
+                        <span className="font-bold">Service Taking Date: </span>
+                        {booking.serviceTakingDate}
+                      </h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
