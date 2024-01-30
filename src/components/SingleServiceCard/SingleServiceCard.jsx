57c3800420870cbf41bc984712cb833fae093a7c
Shishir-Ahmed <shishirahmed790@gmail.com>
2024-01-30 15:59:46 +0600

    fix serviceCard height issue

--- src/components/SingleServiceCard/SingleServiceCard.jsx
+++ src/components/SingleServiceCard/SingleServiceCard.jsx
@@ -13,57 +13,49 @@ const SingleServiceCard = ({ service }) => {
   } = service;
 
   return (
-    <div>
-      <div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 p-4 lg:p-8 xl:p-4 bg-white border rounded-xl h-full shadow-sm">
-          <div className="col-span-1 overflow-hidden">
-            <img
-              className="rounded-xl lg:rounded-none lg:rounded-l-xl w-full h-full object-cover"
-              src={serviceImage}
-              alt={`image for ${serviceName}`}
-            />
-          </div>
-          <div className="col-span-1">
-            <div className="p-4 flex flex-col h-full justify-center">
-              <div className="flex-grow">
-                <div>
-                  <h3 className="text-xl font-bold text-gray-800">
-                    {serviceName}
-                  </h3>
-                  <p className="mt-1 text-sm text-gray-500">
-                    {description.slice(0, 100)}...
-                  </p>
-                  <h6 className="text-[#270f0f] font-semibold mt-1">
-                    ${price}
-                  </h6>
-                </div>
-                <div className="mt-2">
-                  <p className="mt-1 mb-2 font-semibold">{`Service provider's infos`}</p>
-                  <img
-                    className="w-10 h-10 rounded-full"
-                    src={serviceProviderImage}
-                    alt=""
-                  />
-                  <p className="text-sm">
-                    {" "}
-                    <span>Provider: </span>
-                    <span className="font-bold">{serviceProviderName}</span>
-                  </p>
-                  <p className="text-sm">
-                    {" "}
-                    <span>Service Area: </span>
-                    <span className="font-bold">{serviceArea}</span>
-                  </p>
-                </div>
-              </div>
-              <div className="mt-5">
-                <Link to={`/services/${_id}`}>
-                  <button className="text-white text-sm uppercase font-medium btn mt-5 rounded-lg py-3 px-10 bg-[#E39C0D] hover:bg-[#698F3F]">
-                    View Details
-                  </button>
-                </Link>
-              </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2 p-4 lg:p-8 xl:p-4 bg-white border rounded-xl h-full shadow-sm">
+      <div className="col-span-1 overflow-hidden">
+        <img
+          className="rounded-xl lg:rounded-none lg:rounded-l-xl w-full h-full object-cover"
+          src={serviceImage}
+          alt={`image for ${serviceName}`}
+        />
+      </div>
+      <div className="col-span-1">
+        <div className="p-4 flex flex-col h-full justify-center">
+          <div className="flex-grow">
+            <div>
+              <h3 className="text-xl font-bold text-gray-800">{serviceName}</h3>
+              <p className="mt-1 text-sm text-gray-500">
+                {description.slice(0, 100)}...
+              </p>
+              <h6 className="text-[#270f0f] font-semibold mt-1">${price}</h6>
             </div>
+            <div className="mt-2">
+              <p className="mt-1 mb-2 font-semibold">{`Service provider's infos`}</p>
+              <img
+                className="w-10 h-10 rounded-full"
+                src={serviceProviderImage}
+                alt=""
+              />
+              <p className="text-sm">
+                {" "}
+                <span>Provider: </span>
+                <span className="font-bold">{serviceProviderName}</span>
+              </p>
+              <p className="text-sm">
+                {" "}
+                <span>Service Area: </span>
+                <span className="font-bold">{serviceArea}</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-5">
+            <Link to={`/services/${_id}`}>
+              <button className="text-white text-sm uppercase font-medium btn mt-5 rounded-lg py-3 px-10 bg-[#E39C0D] hover:bg-[#698F3F]">
+                View Details
+              </button>
+            </Link>
           </div>
         </div>
       </div>
